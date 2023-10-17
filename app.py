983d952f77d04ac978fd02d042eadc624f8d2656
Kevin Chavez <kjc47@njit.edu>
2023-10-16 22:30:09 -0400

    Adding the Flask website

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Flask!"
+    # Redirect to an external website, e.g., "https://www.example.com"
+    return redirect("https://flask.palletsprojects.com/en/3.0.x/")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-    

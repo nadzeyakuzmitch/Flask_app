3df576c60d07f2998a96e1e9ce9b31ffb044d6eb
Kevin Chavez <kjc47@njit.edu>
2023-11-02 20:32:36 -0400

    deleted the file having it twice

--- models.py
+++ models.py
@@ -4,9 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_script import Manager
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 manager = Manager(app)
 
